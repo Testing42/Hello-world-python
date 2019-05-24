950eccd84813dc1aff97349e94c309a189338811
Testing42 <43222504+Testing42@users.noreply.github.com>
2019-05-24 06:23:56 -0400

    Add files via upload

---
+++ hello python.py
@@ -0,0 +1,11 @@
+# this program says hello and asks for my name
+
+print('hello world')
+print('what is your name') #ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('length of you name is:')
+print(len(myName))
+print('what is your age?') # ask for age
+myAge = input()
+print('You will be ' + str(int(myAge) +1) +' in a year.')
